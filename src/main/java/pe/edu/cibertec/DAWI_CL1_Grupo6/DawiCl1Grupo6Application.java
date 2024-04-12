02c831a956f2121abd08dad891812739a96aa2d7
27jorg16 <146002952+27jorg16@users.noreply.github.com>
2024-04-12 11:04:30 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_Grupo6/DawiCl1Grupo6Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_CL1_Grupo6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl1Grupo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl1Grupo6Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWI_CL1_Grupo6/DawiCl1Grupo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_CL1_Grupo6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl1Grupo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
